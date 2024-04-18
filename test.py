85fb135246e161bb2b5ade0cf1ff6757a363be71
Christian Rojas <crojas0315@gmail.com>
2024-04-18 12:30:50 -0700

    added user input test

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ from openai import OpenAI
 import os
 
 #print(os.environ)
+
+# Get user input
+user_input = input("Enter your message: ")
+
 APIKEY = os.environ.get("OPENAI_SLUG_PRACTICE_API_KEY")
 
 client = OpenAI(
@@ -12,7 +16,7 @@ stream = client.chat.completions.create(
     model = "gpt-3.5-turbo",
     messages = [
          {"role": "system", "content": "You are Jack Sparrow"},
-        {"role": "user", "content": " Hello, what does your compass do?" }
+        {"role": "user", "content": user_input }
        
         ],
     stream=True
