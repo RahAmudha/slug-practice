19c506232aaf2ee7889dda2d2ab3105d3247cbd0
Yaseen <60528365+systemoutput@users.noreply.github.com>
2024-04-22 10:08:39 -0700

    Functional slider with card divs showing or hiding depending on slider value

--- Frontend/src/script.js
+++ Frontend/src/script.js
@@ -1,3 +1,5 @@
+// Scripts for index.html
+
 // Get the submit button element
 const submitButton = document.querySelector('.submit-btn');
 
@@ -10,4 +12,29 @@ submitButton.addEventListener('click', () => {
     // Print the values to the console
     console.log('Prompt 1:', subject_input);
     console.log('Prompt 2:', example_input);
+});
+
+
+// Scripts for practice.html
+
+// Set to cardSlider for easy reference
+const cardSlider = document.querySelector('#card-slider');
+
+// Get value of slider
+cardSlider.addEventListener('input', () => {
+    // Parse the slider's integer value
+    const cardCount = parseInt(cardSlider.value);
+
+    // Set to cardDivs for easy reference
+    const cardDivs = document.querySelectorAll('.card');
+
+    // Iterate through cards and show/hide number of cards based on slider value
+    cardDivs.forEach((card, index) => {
+        if (index < cardCount) {
+            card.style.display = 'grid';
+        } 
+        else {
+            card.style.display = 'none';
+        }
+    });
 });
