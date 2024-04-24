328e8918f3b62925584b136ab2aee9da7210819f
Christian Rojas <crojas0315@gmail.com>
2024-04-23 23:59:32 -0700

    prototyped client api query. successful response

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify,Blueprint
 from app.controllers.api_controller import process_request_simple
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes and origins
 
 @app.route('/api/openai/simple', methods=['POST'])
 def openai_route_simple():
